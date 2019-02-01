2763737ff2d67cd84e386fd18873d9664121a945
Bara Limbong <baralimbong@gmail.com>
2019-02-01 13:25:44 +0700

    create-addressbook

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('address book')
+
+let contacts = [
+  {
+    name: 'bara',
+    phone: '08111',
+    email: 'bara@gmail.com'
+  },
+  {
+    name: 'jo',
+    phone: '08222',
+    email: 'jo@gmail,com'
+  }
+]
